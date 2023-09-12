73682b754e4ed37d4ea64574483777dd7c8605de
Femi Akinyooye <59394789+phemanuel@users.noreply.github.com>
2023-09-12 06:59:30 -0700

    Add files via upload

---
+++ app/Models/Person.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    use HasFactory;
+    protected $table = 'person';
+    protected $fillable = [
+        
+        'name'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+}
